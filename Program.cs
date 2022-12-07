acedd32a3cee803ccfecb35dddba1d5c54b5aecb
anushkachouhan029 <anushkachouhan029>
2022-12-07 23:15:43 +0530

    added uc1 new project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructure_Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Stacks and Queues");
+            LinkedList stack = new LinkedList();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("\n--------stack elements--------");
+            stack.Display();
+        }
+    }
+}
